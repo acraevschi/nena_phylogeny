c4512711357f56c4104888e3de3d1f20b24b54f4
Alexandru Craevschi <craevschi.alexandru@gmail.com>
2024-07-23 15:36:31 +0200

    trial runs data and scripts

--- wide_long_transform.py
+++ wide_long_transform.py
@@ -1,19 +1,30 @@
 import pandas as pd
+import os
 
+# Print out current working directory
+# os.chdir("PhD Papers/na_phylogeny")
 # Open the CSV file
-with open('na_cognates.csv', 'r', encoding="UTF-8") as file:
 
-    data = pd.read_csv('na_cognates.csv', encoding='utf-8')
-    data.columns = ['Language'] + list(data.columns[1:])
+data = pd.read_csv('semitic_cognates.csv', encoding='utf-8')
+data.columns = ['Language'] + list(data.columns[1:])
 
-    # Melt the DataFrame from wide to long format
-    long_df = data.melt(id_vars='Language', var_name='Concept', value_name='Value')
+# Melt the DataFrame from wide to long format
+long_df = data.melt(id_vars='Language', var_name='Concept', value_name='Value')
 
-    num_concepts = len(long_df['Concept'].unique())
-    # Create a dictionary to map the original concepts to unique integers
-    concept_num = {concept: i for i, concept in zip(range(0, num_concepts*101, 100), long_df['Concept'].unique())}
+num_concepts = len(long_df['Concept'].unique())
+# Create a dictionary to map the original concepts to unique integers
+concept_num = {concept: i for i, concept in zip(range(0, num_concepts*101, 100), long_df['Concept'].unique())}
 
-    # Apply the mapping to the 'Concept' column
-    long_df['Value'] = long_df.apply(lambda row: int(row['Value']) + concept_num[row['Concept']] if row['Concept'] in concept_num and row['Value'] != "?" else row['Value'], axis=1)
-    # Write the long format to a new CSV file
-    long_df.to_csv('na_cognates_long.csv', index=False, encoding='utf-8')
+# Apply the mapping to the 'Concept' column
+long_df['Value'] = long_df['Value'].fillna(0)
+
+long_df['Value'] = long_df.apply(
+    lambda row: str(int(row['Value']) + concept_num[row['Concept']]), 
+    axis=1)
+
+concept_num_str = [str(i) for i in concept_num.values()]
+
+long_df.loc[long_df["Value"].isin(concept_num_str), "Value"] = "?"
+
+long_df.to_csv('semitic_cognates_long.csv', index=False, encoding='utf-8')
+
--- write_nexus.py
+++ write_nexus.py
@@ -1,5 +1,46 @@
-from lingpy import *
+from lingpy import convert, get_wordlist
+import pandas as pd
 
-# you might need to change the working directory using package "os" and command "os.chdir('path_to_file')"
-na_wordlist = get_wordlist('na_cognates_long.csv', col="Language", row="Concept")
-convert.strings.write_nexus(na_wordlist, mode="BEAST", filename='na_cognates.nex', ref="value")
+sem_wordlist = get_wordlist('semitic_cognates_long.csv', col="Language", row="Concept")
+
+convert.strings.write_nexus(sem_wordlist, mode="BEAST", filename='semitic_cognates.nex', ref="value", missing="?")
+
+### Modify the nexus file manually as it doesn't display missing data properly
+with open('semitic_cognates.nex', 'r') as nexus_file:
+    nexus_text = nexus_file.readlines()
+
+df = pd.read_csv('semitic_cognates_long.csv')
+df['Concept'] = df['Concept'].str.replace('(', '').str.replace(')', '').str.replace(',', '').str.replace(".", "")
+df['Concept'] = df['Concept'].str.replace(' ', '_')
+df["Language"] = df["Language"].str.replace('_', '').str.replace("'", "")
+
+matrix_begin = nexus_text.index('MATRIX\n') + 1
+matrix_end = nexus_text.index("END;\n") - 1
+
+chars_begin = nexus_text.index('    CHARSTATELABELS\n') + 1
+chars_end = matrix_begin - 2
+
+concepts_ints = list(map(lambda x: x.strip().rstrip(",").split(), nexus_text[chars_begin:chars_end]))
+
+map_concept_ints = {}
+for integer, concept in concepts_ints:
+    if concept not in map_concept_ints.keys():
+        map_concept_ints[concept] = [integer]
+    else:
+        map_concept_ints[concept].append(integer)
+
+for i in range(matrix_begin, matrix_end):
+    lang_name, ints = nexus_text[i].split()
+    missing_vals = df[(df.Language == lang_name) & (df.Value == "?")]["Concept"]
+    new_ints = ints
+    for concept in missing_vals:
+        for integer in map_concept_ints[concept]:
+            integer = int(integer)
+            integer -= 1 # for 0-indexing
+            new_ints = new_ints[:integer] + "?" + new_ints[integer+1:]
+    print(len(new_ints))
+    nexus_text[i] = f"{lang_name}             {new_ints}\n"
+
+
+with open('semitic_cognates.nex', 'w') as nexus_file:
+    nexus_file.write("".join(nexus_text))
